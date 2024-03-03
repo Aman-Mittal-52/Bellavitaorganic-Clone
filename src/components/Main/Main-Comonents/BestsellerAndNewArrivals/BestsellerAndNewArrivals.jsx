4b0ef65917d8085f3d11d3baba96260f2be9af3f
Aman <amanmittle1234@gmail.com>
2024-03-04 03:07:04 +0530

    BestsellerAndNewArrivals Component ViewAllBtn

--- src/components/Main/Main-Comonents/BestsellerAndNewArrivals/BestsellerAndNewArrivals.jsx
+++ src/components/Main/Main-Comonents/BestsellerAndNewArrivals/BestsellerAndNewArrivals.jsx
@@ -1,17 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center } from "@chakra-ui/react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import MyHeading from "./MyHeading";
 import BestsellersBox from "./BestsellersBox";
 import NewArrivalsBox from "./NewArrivalsBox";
 
-function BestsellerAndNewArrivals(){
+function BestsellerAndNewArrivals() {
     const [bestsellerShow, setBestsellerShow] = useState(true)
-    return(
-            <Box p={'2vw'}>
-                <MyHeading bestsellerShow={bestsellerShow} setBestsellerShow={setBestsellerShow}/>
-                {bestsellerShow? <BestsellersBox />:<NewArrivalsBox/>}
-            </Box>
-        )
+    return (
+        <Box p={'2vw'}>
+            <MyHeading bestsellerShow={bestsellerShow} setBestsellerShow={setBestsellerShow} />
+            {bestsellerShow ? <BestsellersBox /> : <NewArrivalsBox />}
+            <Center p={12}>
+                <Link to={bestsellerShow ? "/collections/bestsellers" : '/new-arrivals'}>
+                    <Box className="viewAllBtn" w={'fit-content'} py={4} px={16} border={'1px solid black'} fontSize={14} >VIEW ALL</Box>
+                </Link>
+            </Center>
+        </Box>
+    )
 }
 
 export default BestsellerAndNewArrivals;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react"
+
+import BestsellerAndNewArrivals from './Main-Comonents/BestsellerAndNewArrivals/BestsellerAndNewArrivals'
+
+function Main() {
+    return (
+        <Box id="Main">
+            <BestsellerAndNewArrivals />
+            
+        </Box>
+    )
+}
+
+export default Main;
