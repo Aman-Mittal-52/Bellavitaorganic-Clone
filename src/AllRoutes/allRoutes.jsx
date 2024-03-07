a598104cdd121c25726100013a7a1a9b8578ba40
Aman <amanmittle1234@gmail.com>
2024-03-07 20:33:24 +0530

    BAsicPage component

--- src/AllRoutes/allRoutes.jsx
+++ src/AllRoutes/allRoutes.jsx
@@ -1,18 +1,25 @@
-import Users from "../pages/Users";
+// import Users from "../pages/Users";
 import Home from "../pages/Home";
-import Contact from "../pages/Contact";
-import Login from "../pages/Login";
-import About from "../pages/About";
-import {Routes ,Route} from 'react-router-dom';
+// import Contact from "../pages/Contact";
+// import Login from "../pages/Login";
+// import About from "../pages/About";
+import { Routes, Route } from 'react-router-dom';
+import BasicPage from "../pages/BasicPage";
+import NotFound from "../pages/NotFoundPage";
 
-function AllRoutes(){
-    return(
+function AllRoutes() {
+    return (
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/About" element={<About/>}/>
-            <Route path="/Contact" element={<Contact/>}/>
-            <Route path="/Users" element={<Users/>}/>
-            <Route path="/Login" element={<Login/>}/>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/About" element={<About />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/Users" element={<Users />} />
+            <Route path="/Login" element={<Login />} /> */}
+            <Route path="*" element={
+                <BasicPage>
+                    <NotFound />
+                </BasicPage>
+            } />
         </Routes>
     )
 }
